ec506ef7d47b95da5d303ff4e944a51c8396c9dc
yulia_vishnevskaya <yulia_vishnevskaya@tchdm.live>
2022-12-30 18:47:49 +0200

    delete anim

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 import {initLazyLoadFonts} from "./components/initLazyLoadFonts";
 import {initMobMenu} from "./components/initMobMenu";
 import {initScrollLocker} from "./components/menus/initScrollLocker";
-import {initScrollAnimation} from "./components/initScrollAnimation";
 import { initScrollDownButton } from './components/initSrollDown';
 import { initSlider } from './components/initSlider';
 
@@ -13,7 +12,6 @@ function handleOnLoadPage() {
     initLazyLoadFonts();
     initMobMenu();
     initScrollLocker();
-    initScrollAnimation();
     initScrollDownButton();
     initSlider();
 }
